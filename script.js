81a47edd1308f30031f274ff6f74ac36c40dc038
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-18 21:28:17 -0500

    fix call/px add

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function createBoxes() {
         for(let j = 0; j < 4; j++) {
             const box = document.createElement('div');
             box.classList.add('box');
-            box.style.backgroundPosition = `${-j * 125} ${-i * 125}`
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
             boxesContainer.appendChild(box)
         }
     }
 }
+
+createBoxes()

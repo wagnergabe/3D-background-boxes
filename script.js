dfc73aa3c56aed4cbeadec7d8e5cf730eb7e4ab6
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-18 21:34:05 -0500

    toggle animation working

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const boxesContainer = document.getElementById('boxes');
 const btn = document.getElementById('btn');
 
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
 function createBoxes() {
     for(let i = 0; i < 4; i++) {
         for(let j = 0; j < 4; j++) {
